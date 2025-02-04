9be1ee41356a60a94d1f881203fe5df2897d1499
nat-2006 <negardner@email.neit.edu>
2025-02-04 01:08:33 -0500

    practice

---
+++ pratice.py
@@ -0,0 +1,24 @@
+
+#var and data types
+x = 10        # integer
+y = 3.14      # float
+name = "Alex" # string
+is_ready = True # boolean
+age = 18
+
+#conditions
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# For loop
+for i in range(5):
+    print(i)  # Prints 0 to 4
+
+# While loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
